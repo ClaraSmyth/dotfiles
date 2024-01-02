452443b3ae7ae9b941b8f62885e2f6990b0ffa65
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2024-01-02 17:24:11 +0000

    Update nnn plugin env var

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,8 +11,8 @@ fish_add_path '/home/clara/.cargo/bin/'
 
 set -Ux HELIX_RUNTIME '/home/clara/.local/share/helix/runtime'
 set -Ux NNN_FIFO '/tmp/nnn.fifo'
-set -Ux NNN_PLUG 'p:preview-tui'
-set -Ux EDITOR 'nvim'
+set -Ux NNN_PLUG 'p:preview-tui;d:dragdrop'
+set -Ux EDITOR nvim
 
 alias z='zellij'
 alias hx='helix'
