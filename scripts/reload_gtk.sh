981120694f886562d50dbb8192e7a023900e1450
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2024-02-22 18:28:21 +0000

    Added reload_gtk script

---
+++ scripts/reload_gtk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This just toggles a gtk theme between a 
+# symlink and the actual theme to reload it
+
+# Using a symlink prevents color shift flash
+
+gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark-alt
+gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
