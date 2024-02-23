b10107bfb5677fc9230e6c17627922d0ada3209a
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2024-02-23 20:13:28 +0000

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,5 @@ set -x EDITOR hx
 
 starship init fish | source
 
-# pnpm
-set -gx PNPM_HOME "/home/clara/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
+alias yay="paru"
+alias pnpx="pnpm dlx"
