350bdafb7e6c1b1f0e9dce885780d385e3957ee1
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2024-09-15 21:26:56 +0100

    Added zoxide to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ set -x HELIX_RUNTIME ~/src/helix/runtime
 set -x EDITOR hx
 set -x SWWW_TRANSITION wipe
 
-starship init fish | source
-
 alias pnpx="pnpm dlx"
+
+starship init fish | source
+zoxide init fish | source
